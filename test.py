38297ae53e3bba1b443e2dddfec8cf2f9c298043
Keroshi <bgathendu@live.com>
2024-07-16 03:57:34 +0300

    Added more files

---
+++ test.py
@@ -0,0 +1,10 @@
+from transformers import GPT2Tokenizer, GPT2Model
+import warnings
+
+warnings.filterwarnings("ignore")
+
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2Model.from_pretrained('gpt2')
+text = "Replace me by any text you'd like."
+encoded_input = tokenizer(text, return_tensors='pt')
+output = model(**encoded_input)
